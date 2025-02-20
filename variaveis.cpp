91f47d348cb18405d971a7672bfbe195109d87ac
Gabriel H. Finotti <finotti.dev@gmail.com>
2025-02-20 12:55:50 -0300

    variaveis

---
+++ variaveis.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+int main()
+{
+    int x1{0};
+    char letraUnica('a');
+    float y = 3.20;
+    double z = 3.30434343;
+    std::string nome = {"Gabriel"};
+    std::string test = "";
+
+    std::cout << "O valor de x é: " << x1 << std::endl;
+    std::cout << "O valor de letraUnica é: " << letraUnica << std::endl;
+    std::cout << "O valor de y é: " << y << std::endl;
+    std::cout << "O valor de z é: " << z << std::endl;
+
+    return 0;
+}
